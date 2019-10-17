cb6fb2434cb8ab4954233890102be3389bcc9e77
Rishub Podar <rishubpodar@wirelessprv-10-193-63-166.near.illinois.edu>
2019-10-17 17:02:29 -0500

    Can read packets

--- capture.c
+++ capture.c
@@ -1,62 +1,59 @@
-#include <pcap.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <pcap.h>
+#include <netinet/in.h>
+#include <net/ethernet.h>
 
-#define ERR_BUF_SIZE 2048
+/* This function can be used as a callback for pcap_loop() */
+void another_callback(u_char *arg, const struct pcap_pkthdr* pkthdr, 
+        const u_char* packet) 
+{ 
+    int i=0; 
+    static int count=0; 
+ 
+    printf("Packet Count: %d\n", ++count);    /* Number of Packets */
+    printf("Recieved Packet Size: %d\n", pkthdr->len);    /* Length of header */
+    printf("Payload:\n");                     /* And now the data */
+    for(i=0;i<pkthdr->len;i++) { 
+        if(isprint(packet[i]))                /* Check if the packet data is printable */
+            printf("%c",packet[i]);          /* Print it */
+        else
+            printf(".",packet[i]);          /* If not print a . */
+        if((i%16==0 && i!=0) || i==pkthdr->len-1) 
+            printf("\n"); 
+    }
+}
 
-/**
- * Sources:
- * 	http://www.cs.colby.edu/maxwell/courses/tutorials/maketutor/
- *  https://www.tcpdump.org/pcap.html
- *
- */
+int main(int argc, char **argv) {
+    pcap_t *handle;
+    char error_buffer[PCAP_ERRBUF_SIZE];
+    char *dev = "en0";
+    int snapshot_len = 1028;
+    int promiscuous = 1;
+    int timeout = 100000;
+    struct bpf_program filter;
+    char filter_exp[] = "port 80";
+    bpf_u_int32 subnet_mask, ip;
 
-int main() {
+    if (pcap_lookupnet(dev, &ip, &subnet_mask, error_buffer) == -1) {
+        printf("Could not get information for device: %s\n", dev);
+        ip = 0;
+        subnet_mask = 0;
+    }
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, error_buffer);
+    if (handle == NULL) {
+        printf("Could not open %s - %s\n", dev, error_buffer);
+        return 2;
+    }
+    if (pcap_compile(handle, &filter, filter_exp, 0, ip) == -1) {
+        printf("Bad filter - %s\n", pcap_geterr(handle));
+        return 2;
+    }
+    if (pcap_setfilter(handle, &filter) == -1) {
+        printf("Error setting filter - %s\n", pcap_geterr(handle));
+        return 2;
+    }
 
-	char* dev, errbuf[ERR_BUF_SIZE];
-
-	dev = pcap_lookupdev(errbuf);
-	if (dev == NULL) {
-		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-		exit(2);
-	}
-	printf("Device: %s\n", dev);
-
-	pcap_t* handle = pcap_open_live(dev, BUFSIZ /* Defined in <pcap.h> */, 1, 1000, errbuf);
-
-	if (handle == NULL) {
-		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-		exit(2);
-	}
-	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-	if (handle == NULL) {
-		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-		return(2);
-	}
-
-	struct bpf_program fp;		/* The compiled filter */
-	char filter_exp[] = "len >= 0";	/* https://www.tcpdump.org/manpages/pcap-filter.7.html */
-	bpf_u_int32 mask;		/* Our netmask */
-	bpf_u_int32 net;		/* Our IP */
-	struct pcap_pkthdr header;	/* The header that pcap gives us */
-	const u_char *packet;		/* The actual packet */
-		
-	/* Compile and apply the filter */
-	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-		return(2);
-	}
-	if (pcap_setfilter(handle, &fp) == -1) {
-		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-		return(2);
-	}
-	/* Grab a packet */
-	while (1) {
-		packet = pcap_next(handle, &header);
-		/* Print its length */
-		printf("Jacked a packet with length of [%d]\n", header.len);
-	}
-	/* And close the session */
-	pcap_close(handle);
-	return(0);
+    pcap_loop(handle, 0, another_callback, NULL);
+    pcap_close(handle);
+    return 0;
 }
